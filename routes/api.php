72844b6aae1b6e287bb7e545db66a8cf1cad33d0
kavindu919 <jayakodyk2@gmail.com>
2025-07-14 10:28:01 +0530

    define routes for the students table

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,4 +8,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->group(function () {
 
     Route::middleware('auth:sanctum')->group(function () {});
+
+    Route::group(['prefix' => 'student'], function () {
+        Route::get('/getallstudents', [StudentController::class, 'getAllStudents'])->name('get-allstudents');
+        Route::get('/getsinglestudent', [StudentController::class, 'getSingleStudent'])->name('get-singlestudent');
+        Route::post('/addstudent', [StudentController::class, 'addStudents'])->name('add-students');
+        Route::post('/editstudent', [StudentController::class, 'editStudents'])->name('edit-students');
+        Route::post('/deletestudent', [StudentController::class, 'softDeleteStudent'])->name('delete-students');
+    });
 });
